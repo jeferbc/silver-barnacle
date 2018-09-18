323a6774c94b2aea6a92e53bf001971ae8332869
Germán Escobar <german.escobarc@gmail.com>
2018-09-18 17:35:16 -0500

    Implement some integration test

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,44 @@
+const puppeteer = require("puppeteer");
+const mongoose = require("mongoose");
+const app = require("../../app");
+
+let server;
+let page;
+let browser;
+const width = 1920;
+const height = 1080;
+beforeAll(async () => {
+  server = app.listen(3000);
+
+  browser = await puppeteer.launch({
+    headless: true,
+    // slowMo: 40,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+});
+
+beforeEach(async () => {
+  for (var i in mongoose.connection.collections) {
+    await mongoose.connection.collections[i].remove({});
+  }
+});
+
+afterAll(async () => {
+  server.close();
+  await mongoose.disconnect();
+  browser.close();
+});
+
+test("user can register and login", async () => {
+  await page.goto("http://localhost:3000/");
+  await page.click('a[href="/register"]');
+  await page.waitFor('input[id=email]');
+  await page.type("input[id=email]", "pedro@gmail.com");
+  await page.type("input[id=password]", "test1234");
+  const nav = page.waitForNavigation();
+  await page.click("button[type=submit]");
+  await nav;
+  expect(page.url()).toMatch(/login$/);
+}, 5000);
---
+++ tests/integration/surveys.test.js
@@ -0,0 +1,61 @@
+const puppeteer = require("puppeteer");
+const mongoose = require("mongoose");
+const Poll = require("../../models/Poll");
+const app = require("../../app");
+
+let server;
+let page;
+let browser;
+const width = 1920;
+const height = 1080;
+beforeAll(async () => {
+  server = app.listen(3000);
+
+  browser = await puppeteer.launch({
+    headless: true,
+    // slowMo: 80,
+    args: [`--window-size=${width},${height}`]
+  });
+  page = await browser.newPage();
+  await page.setViewport({ width, height });
+});
+
+beforeEach(async () => {
+  for (var i in mongoose.connection.collections) {
+    await mongoose.connection.collections[i].remove({});
+  }
+});
+
+afterAll(async () => {
+  server.close();
+  await mongoose.disconnect();
+  browser.close();
+});
+
+test("should list polls and vote", async () => {
+  await Poll.create({
+    name: "Poll 1",
+    description: "Description 1",
+    options: [
+      { text: "Option 1" },
+      { text: "Option 2" }
+    ]
+  });
+
+  await page.goto("http://localhost:3000/");
+  let nav = page.waitForNavigation();
+  await page.click(".poll a");
+  await nav;
+
+  await page.click("input[type=radio]");
+  nav = page.waitForNavigation();
+  await page.click("button[type=submit]");
+  await nav;
+
+  const spans = await page.$$(".options .option .option-label span");
+  const s1 = await page.evaluate(span => span.innerText, spans[0]);
+  expect(s1).toBe("1 votes");
+
+  const s2 = await page.evaluate(span => span.innerText, spans[1]);
+  expect(s2).toBe("0 votes");
+})
